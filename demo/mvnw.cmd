f85dddda4876ccd75b0ed2c00e7dd9efe207ef3b
wiggapony0925 <ninjeff06@gmail.com>
2023-07-15 23:31:22 -0400

    updated the xml file for the configuration of the use of vaadin add-ons and components

--- demo/mvnw.cmd
+++ demo/mvnw.cmd
@@ -9,7 +9,7 @@
 @REM
 @REM    https://www.apache.org/licenses/LICENSE-2.0
 @REM
-@REM Unless required by applicable law or agreed to in writing,
+@REM Unless required bya pplicable law or agreed to in writing,
 @REM software distributed under the License is distributed on an
 @REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 @REM KIND, either express or implied.  See the License for the
